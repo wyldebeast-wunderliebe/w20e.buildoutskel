63780b3538b4de0a699141425948fb0b8aaf5a7e
Duco Dokter <dokter@w20e.com>
2012-07-23 21:06:37 +0200

    fixed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = [
     ]
 
 setup(name='w20e.buildoutskel',
-      version='0.0',
+      version='1.0.0a',
       description='Paster template',
       long_description="",
       classifiers=[
